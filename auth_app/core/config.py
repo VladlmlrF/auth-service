6c6f441091e50b146497126634eac8b237d6884e
VladlmlrF <vladimirfalin84@gmail.com>
2024-10-27 16:03:30 +0700

    feat: Created token schema auth settings

--- auth_app/core/config.py
+++ auth_app/core/config.py
@@ -24,3 +24,13 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+class AuthSettings(BaseSettings):
+    private_key_path: Path = BASE_DIR / "certs" / "private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
+
+
+auth_settings = AuthSettings()
