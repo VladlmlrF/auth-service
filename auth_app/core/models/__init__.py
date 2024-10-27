1c9e6980c2271beaffe858cb45d6d6d725df2207
VladlmlrF <vladimirfalin84@gmail.com>
2024-10-27 14:25:03 +0700

    feat: Added imports

---
+++ auth_app/core/models/__init__.py
@@ -0,0 +1,14 @@
+__all__ = (
+    "Base",
+    "db_helper",
+    "Service",
+    "User",
+    "UserRole",
+)
+
+
+from .base import Base
+from .db_helper import db_helper
+from .service import Service
+from .user import User
+from .user_role import UserRole
